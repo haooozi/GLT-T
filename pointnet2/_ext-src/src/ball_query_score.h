d64e9344730b4df37309f4c6c3e81ef7cff979fd
Jiahao Nie <jhnie@hdu.edu.cn>
2023-10-01 11:26:32 +0800

    Update

--- pointnet2/_ext-src/src/ball_query_score.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <torch/extension.h>
-
-at::Tensor ball_query_score(at::Tensor new_xyz, at::Tensor xyz, at::Tensor score, const float radius,
-                      const int nsample);
